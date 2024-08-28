ce073d251a7be4b9b8e0ae7818ce89dee781df2e
Bethan Cottell <cottellbc@gmail.com>
2024-08-28 10:37:57 +0000

    add validation to check for exactly 6 values in data provided

--- run.py
+++ run.py
@@ -16,18 +16,41 @@ sales = SHEET.worksheet('sales')
 
 data = sales.get_all_values()
 
-print(data)
+# print(data) commented out as working
 
 def get_sales_data():
     """
     Get sales figures input from the user
+    split turns string to a list by splitting 
+    the values at the comma
+    the backslash n gives an extra line of space below.
     """
     print("please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+   
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into intergers. 
+    Raise ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    e shorthand for error in python!
+    """
+    
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 
 get_sales_data()
