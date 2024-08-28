e78c596b2646de4c744ba31ca5ba211461644f99
Bethan Cottell <cottellbc@gmail.com>
2024-08-28 13:30:57 +0000

    successfully updated all three worksheets

--- run.py
+++ run.py
@@ -136,14 +136,33 @@ def get_last_5_entries_sales():
     """
 
     sales = SHEET.worksheet("sales")
- 
-columns = []
-for ind in range(1, 7):
-    column = sales.col_values(ind)
-    columns.append(column[-5:])
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
 
-return columns
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    because we know our stock columns will always have 5 items
+    in them this expression could work too:
 
+    sum(int_column) / 5
+
+    however the way weve done it here is using length when the
+    length of the list may vary
+    """
+    print("Calculating stock data . . .\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 def main():
     """
@@ -154,7 +173,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("WELCOME TO LOVE SANDWICHES DATA AUTOMATION")
-# main()
-sales_columns = get_last_5_entries_sales()
+main()
+
